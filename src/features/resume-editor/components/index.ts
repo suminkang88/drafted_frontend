0a5a6d4eacccecfa5bc4ae6e1a935fb9d7ca01f0
sumin <smkl1004@snu.ac.kr>
2025-07-22 15:35:08 +0900

    sdfsf

--- src/features/resume-editor/components/index.ts
+++ src/features/resume-editor/components/index.ts
@@ -2,8 +2,6 @@ export { AISuggestionCard } from './AISuggestionCard';
 export { ContentInputBox } from './ContentInputBox';
 export { default as ChatInput } from '@/features/resume-editor/components/ChatInput';
 export { default as ToggledSelectedActivityCard } from '@/features/resume-editor/components/ToggledSelectedActivityCard';
-export {
-  default as AutoSaved,
-  useDebounceSave,
-} from '@/features/resume-editor/components/AutoSaved';
+export { default as AutoSaved } from '@/features/resume-editor/components/AutoSaved';
+export { useDebounceSave } from '@/features/resume-editor/components/AutoSaved';
 export { default as DragContentCard } from '@/features/resume-editor/components/DragContentCard';
