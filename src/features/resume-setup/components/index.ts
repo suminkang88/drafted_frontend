7b52a20b634f8e68a40085dd08e3f35097f8f1fc
sumin <smkl1004@snu.ac.kr>
2025-07-22 16:19:44 +0900

    testing after merge

--- src/features/resume-setup/components/index.ts
+++ src/features/resume-setup/components/index.ts
@@ -2,3 +2,6 @@ export { default as SideBar } from './SideBar';
 export { default as EventRecommendationCard } from './EventRecommendationCard';
 export { default as ActivityAddModal } from './Modal/ActivityAddModal';
 export { default as ActivitySearchModal } from './Modal/ActivitySearchModal';
+export { default as BasicInfoInputCard } from '@/features/resume-setup/components/BasicInfoInputCard';
+export { default as QuestionInputCard } from '@/features/resume-setup/components/QuestionInputCard';
+export { default as StepText } from '@/features/resume-setup/components/StepText';
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SetupTestPage from '@/pages/SetupTestPage';
+//import SetupTestPage from '@/pages/SetupTestPage';
 import TestPage from '@/pages/TestPage';
 
 const AppRouter = () => {
@@ -8,7 +8,7 @@ const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TestPage />} />
-        <Route path="/test" element={<SetupTestPage />} />
+        {/*<Route path="/test" element={<SetupTestPage />} />*/}
       </Routes>
     </BrowserRouter>
   );
