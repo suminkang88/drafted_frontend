165150d8f7dc52cb3368ec2621d4e27d0de140ad
minji <bona718@snu.ac.kr>
2025-07-30 15:08:33 +0900

    feat: resume history page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //import SetupTestPage from '@/pages/SetupTestPage';
 import TestPage from '@/pages/TestPage';
+// import ActivityRecommendationPage from '@/features/resume-setup/ActivityRecommendPage';
+import ResumeHistoryPage from '@/features/resume-history/ResumeHistoryPage';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<TestPage />} />
-        {/*<Route path="/test" element={<SetupTestPage />} />*/}
+        <Route path="/applications" element={<ResumeHistoryPage />} />
       </Routes>
     </BrowserRouter>
   );
