8c7c168781e50eb6accf3bfe4ae805ca09341806
sumin <smkl1004@snu.ac.kr>
2025-08-04 16:16:07 +0900

    auth api 생성 (auth/api/userApi.ts, auth/hooks/u seUser.ts)

---
+++ src/features/auth/api/userApi.ts
@@ -0,0 +1,22 @@
+// src/features/auth/api/userApi.ts
+import axios from '@/api/axios';
+
+export const createUser = async (data: any) => {
+  const response = await axios.post('/users/', data);
+  return response.data;
+};
+
+export const fetchUser = async (userId: string | number) => {
+  const response = await axios.get(`/users/${userId}/`);
+  return response.data;
+};
+
+export const updateUser = async (userId: string | number, data: any) => {
+  const response = await axios.put(`/users/${userId}/`, data);
+  return response.data;
+};
+
+export const deleteUser = async (userId: string | number) => {
+  const response = await axios.delete(`/users/${userId}/`);
+  return response.data;
+};
